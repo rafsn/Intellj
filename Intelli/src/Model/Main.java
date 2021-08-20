e40361bb10edac021a3e725c1c3fb901dde0749e
Rafasantos <santos_rafa1973@hotmail.com>
2021-08-20 20:05:05 -0300

    Primeiro commit

---
+++ Intelli/src/Model/Main.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Main {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+//        int a = 5;
+//        int b = 3;
+//        System.out.println("Olá rafa " + (a+b));
+    }
+}
